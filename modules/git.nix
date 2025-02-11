e416087bfa2a0fd9e0e713ea29dcf7347e8676d4
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-02-12 01:04:25 +0400

    feat(module): git

---
+++ modules/git.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  lib,
+  specialArgs,
+  ...
+}: let
+  inherit (specialArgs) fullName email;
+  cfg = config.host.git;
+in
+  with lib; {
+    options = {
+      host.git.enable = mkEnableOption "git";
+    };
+
+    config = mkIf cfg.enable {
+      programs = {
+        git = {
+          enable = true;
+          aliases = {
+            fix = "commit --fixup";
+            pufowile = "push --force-with-lease";
+            sl = "log --oneline --decorate --graph";
+            sla = "sl --all";
+          };
+          extraConfig = {
+            branch.autoSetupRebase = "always";
+            commit.gpgSign = true;
+            core = {
+              autocrlf = false;
+              editor = "nvim";
+              eol = "lf";
+              excludesFile = "../dotfiles/git/.gitignore_global";
+            };
+            diff.submodule = "log";
+            fetch.prune = true;
+            gpg.format = "ssh";
+            init.defaultBranch = "main";
+            pull = {
+              default = "current";
+              rebase = true;
+            };
+            push = {
+              default = "current";
+              recurseSubmodules = "on-demand";
+            };
+            status.submoduleSummary = true;
+            rebase.autoSquash = true;
+            rerere.enabled = true; # REuse REcorded REsolution
+          };
+          userName = fullName;
+          userEmail = email;
+          signing.key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        };
+      };
+    };
+  }
