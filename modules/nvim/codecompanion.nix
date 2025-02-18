cf087a6d59b88cf79423b03f98b49d3962ffd28b
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-02-18 19:18:30 +0400

    feat(nvim): AI powered coding using codecompanion.nvim
    I've configured it to interface with OpenRouter.ai which is a generic
    interface for interacting with a plethora of LLMs and handles everything
    from billing, provider selection, model selection and beyond.

--- modules/nvim/codecompanion.nix
+++ modules/nvim/codecompanion.nix
@@ -1,12 +1,81 @@
-{ ... }:
+{ specialArgs, ... }:
+let
+  inherit (specialArgs) hostname username;
+  model = "google/gemini-2.0-pro-exp-02-05:free";
+in
 {
+  sops.secrets.openrouter_api_key = {
+    format = "binary";
+    sopsFile = ../../hosts/${hostname}/users/${username}/secrets/openrouter_api_key;
+    path = "%r/openrouter_api_key";
+  };
+
   programs.nixvim = {
     keymaps = [
-
+      {
+        mode = "n";
+        key = "<leader>cca";
+        action = "<cmd>CodeCompanionActions<cr>";
+        options = {
+          desc = "CodeCompanion Actions";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>ccc";
+        action = "<cmd>CodeCompanionChat<cr>";
+        options = {
+          desc = "CodeCompanion Chat";
+        };
+      }
     ];
 
+    # TODO: seems code completion with nvim-cmp doesn't work yet
     plugins = {
-      codecompanion.enable = true;
+      codecompanion = {
+        enable = true;
+        settings = {
+          adapters = {
+            openrouter.__raw = ''
+              function()
+                return require('codecompanion.adapters').extend('openai', {
+                  name = "openrouter",
+                  url = "https://openrouter.ai/api/v1/chat/completions",
+                  env = {
+                    api_key = "cmd: cat $XDG_RUNTIME_DIR/openrouter_api_key",
+                    model = "schema.model.default",
+                  },
+                  formatted_name = "OpenRouter",
+                  schema = {
+                    model = {
+                      default = "${model}"
+                    },
+                  },
+                })
+              end
+            '';
+          };
+          display = {
+            chat = {
+              show_settings = true;
+            };
+            diff = {
+              provider = "mini_diff";
+            };
+          };
+          strategies = {
+            agent = {
+              adapter = "openrouter";
+            };
+            chat = {
+              adapter = "openrouter";
+            };
+            inline = {
+              adapter = "openrouter";
+            };
+          };
+        };
+      };
     };
   };
 }
