e468103f8c753cba6e5aa239c4fd095b2af7cb0d
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-03-14 02:32:37 +0400

    feat(zsh): enable syntax highlighting

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -22,11 +22,37 @@ with lib;
           "--cmd cd"
         ];
       };
+
       zsh = {
         enable = true;
         autosuggestion.enable = true;
-        history.ignoreAllDups = true;
+        history = {
+          ignoreAllDups = true;
+          ignorePatterns = [
+            "cd *"
+            "pkill *"
+            "rm *"
+          ];
+        };
         historySubstringSearch.enable = true;
+        initExtra = ''
+          fastfetch
+
+          # automatically prune branches both local and remote
+          function gpb {
+            git checkout "$(git_main_branch)"
+            git fetch
+            git remote prune origin
+            git branch --merged | grep -vE "$(git_main_branch)|$(git_develop_branch)" | xargs -r git branch -d
+          }
+
+          # git remove submodule
+          function grms {
+            git rm $PWD/$1
+            rm -rf $PWD/.git/modules/$1
+            git config --remove-section submodule.$1
+          }
+        '';
         oh-my-zsh = {
           enable = true;
           plugins = [
@@ -49,30 +75,13 @@ with lib;
             exec systemd-cat -t uwsm_start uwsm start default
           fi
         '';
+        syntaxHighlighting.enable = true;
         shellAliases = {
           a = "artisan";
           tinker = "artisan tinker";
           cat = "bat --paging=never";
           kamal = "docker run -it --rm -v '$PWD:/workdir' -v '$SSH_AUTH_SOCK:/ssh-agent' -v /var/run/docker.sock:/var/run/docker.sock -e 'SSH_AUTH_SOCK=/ssh-agent' ghcr.io/basecamp/kamal:latest";
         };
-        initExtra = ''
-          fastfetch
-
-          # automatically prune branches both local and remote
-          function gpb {
-            git checkout "$(git_main_branch)"
-            git fetch
-            git remote prune origin
-            git branch --merged | grep -vE "$(git_main_branch)|$(git_develop_branch)" | xargs -r git branch -d
-          }
-
-          # git remove submodule
-          function grms {
-            git rm $PWD/$1
-            rm -rf $PWD/.git/modules/$1
-            git config --remove-section submodule.$1
-          }
-        '';
       };
     };
   };
