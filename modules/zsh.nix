abfba696fb90b5881a2c9d3bfc48bb5d84c96cdd
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-03-28 23:50:15 +0400

    refactor(git): extract custom git functions for Zsh to git module

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -37,21 +37,6 @@ with lib;
         historySubstringSearch.enable = true;
         initExtra = ''
           fastfetch
-
-          # automatically prune branches both local and remote
-          function gpb {
-            git checkout "$(git_main_branch)"
-            git fetch
-            git remote prune origin
-            git branch --merged | grep -vE "$(git_main_branch)|$(git_develop_branch)" | xargs -r git branch -d
-          }
-
-          # git remove submodule
-          function grms {
-            git rm $PWD/$1
-            rm -rf $PWD/.git/modules/$1
-            git config --remove-section submodule.$1
-          }
         '';
         oh-my-zsh = {
           enable = true;
@@ -63,7 +48,6 @@ with lib;
             "docker"
             "docker-compose"
             "git"
-            "history"
             "laravel"
             "rails"
             "ssh-agent"
