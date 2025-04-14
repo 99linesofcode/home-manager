4ece5ab77e8d3aa470745406424df747e5725591
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-04-14 15:23:20 +0400

    feat(hyprlock): display date and time on lock screen

--- modules/hyprland/hyprlock.nix
+++ modules/hyprland/hyprlock.nix
@@ -16,6 +16,22 @@ with lib;
     programs = {
       hyprlock = {
         enable = true;
+        settings = {
+          label = [
+            {
+              text = "cmd[update:1000] echo $TIME";
+              font_size = "128";
+              font_family = "Noto Sans";
+              position = "200, 300";
+            }
+            {
+              text = "cmd[update:1000] echo $(date '+%A, %B %d')";
+              font_size = "64";
+              font_family = "Noto Sans";
+              position = "200, 200";
+            }
+          ];
+        };
       };
     };
   };
