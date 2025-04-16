409b9dc4022eae256ec34dc22f3b33f046fe08e1
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-04-16 22:50:35 +0400

    feat(firefox): completely disable Pocket for added performance boost

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -170,6 +170,20 @@ with lib;
 
             # TODO: required for nvidia-vaapi-driver, how to toggle on nixos-config value 🤔
             "media.ffmpeg.vaapi.enabled" = true;
+
+            # completely disable Pocket
+            "extensions.pocket.enabled" = false;
+            "extensions.pocket.api" = "0.0.0.0";
+            "extensions.pocket.loggedOutVariant" = "";
+            "extensions.pocket.oAuthConsumerKey" = "";
+            "extensions.pocket.onSaveRecs" = false;
+            "extensions.pocket.onSaveRecs.locales" = "";
+            "extensions.pocket.showHome" = false;
+            "extensions.pocket.site" = "0.0.0.0";
+            "browser.newtabpage.activity-stream.pocketCta" = "";
+            "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+            "services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket" =
+              false;
           };
         };
       };
