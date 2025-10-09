95a72bd04215eab089caa035e47db74a88fa1c1d
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2025-10-10 00:19:52 +0200

    feat(alias): run both artisan and composer in docker container if available

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -43,21 +43,21 @@ with lib;
         initContent = ''
           fastfetch
 
-          function a() {
+          function p() {
             if [ -f docker-compose.yml ] || [ -f docker-compose.yaml ]; then
               if docker ps -f "name=php" -f "publish=80" --format "{{.ID}}" | grep -q .; then
-                docker compose exec php php artisan "$@"
+                docker compose exec php $@
                 return
               fi
             fi
 
             # TODO: local PHP should probably take precedence when dnsmasq is setup correctly
             if command -v php >/dev/null 2>&1; then
-              php artisan "$@"
+              $@
               return
             fi
 
-            echo "Failed to run artisan. Could not find PHP or PHP docker container."
+            echo "Failed to run $@. Could not find PHP or PHP docker container."
             return 1
           }
         '';
@@ -67,13 +67,10 @@ with lib;
             "asdf"
             "colorize"
             "colored-man-pages"
-            "composer"
             "docker"
             "docker-compose"
             "fzf"
             "git"
-            "laravel"
-            "rails"
             "ssh-agent"
           ];
           theme = "juanghurtado";
@@ -85,8 +82,12 @@ with lib;
         '';
         syntaxHighlighting.enable = true;
         shellAliases = {
-          tinker = "a tinker";
+          a = "p php artisan";
+          artisan = "a";
           cat = "bat --paging=never";
+          c = "p composer";
+          composer = "c";
+          tinker = "a tinker";
           kamal = "docker run -it --rm -v '$PWD:/workdir' -v '$SSH_AUTH_SOCK:/ssh-agent' -v /var/run/docker.sock:/var/run/docker.sock -e 'SSH_AUTH_SOCK=/ssh-agent' ghcr.io/basecamp/kamal:latest";
         };
       };
