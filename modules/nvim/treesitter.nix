6449e2a5ee485602444701ec92a88bca9790f655
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-01-13 00:02:30 +0100

    refactor(nvim): treesitter-textobjects options were moved into treesitter-textobjects.settings

--- modules/nvim/treesitter.nix
+++ modules/nvim/treesitter.nix
@@ -7,10 +7,9 @@
     plugins = {
       treesitter = {
         enable = true;
-        settings = {
-          indent.enable = true;
-          highlight.enable = true;
-        };
+        folding.enable = true;
+        highlight.enable = true;
+        indent.enable = true;
         nixvimInjections = true;
       };
       treesitter-context = {
@@ -20,52 +19,54 @@
           trim_scope = "outer";
         };
       };
-      treesitter-refactor.enable = true;
+      # treesitter-refactor.enable = true; # FIXME: wouldnt build for 4ebc11dad54b7e5b8a46c6edb2533852e99ec566bbe10836b2d6dba89c9c2e3c
       treesitter-textobjects = {
         enable = true;
-        select = {
-          enable = true;
-          lookahead = true;
-          keymaps = {
-            "aa" = "@parameter.outer";
-            "ia" = "@parameter.inner";
-            "af" = "@function.outer";
-            "if" = "@function.inner";
-            "ac" = "@class.outer";
-            "ic" = "@class.inner";
-            "ii" = "@conditional.inner";
-            "ai" = "@conditional.outer";
-            "il" = "@loop.inner";
-            "al" = "@loop.outer";
-            "at" = "@comment.outer";
+        settings = {
+          select = {
+            enable = true;
+            lookahead = true;
+            keymaps = {
+              "aa" = "@parameter.outer";
+              "ia" = "@parameter.inner";
+              "af" = "@function.outer";
+              "if" = "@function.inner";
+              "ac" = "@class.outer";
+              "ic" = "@class.inner";
+              "ii" = "@conditional.inner";
+              "ai" = "@conditional.outer";
+              "il" = "@loop.inner";
+              "al" = "@loop.outer";
+              "at" = "@comment.outer";
+            };
           };
-        };
-        move = {
-          enable = true;
-          gotoNextStart = {
-            "]m" = "@function.outer";
-            "]]" = "@class.outer";
+          move = {
+            enable = true;
+            gotoNextStart = {
+              "]m" = "@function.outer";
+              "]]" = "@class.outer";
+            };
+            gotoNextEnd = {
+              "]M" = "@function.outer";
+              "][" = "@class.outer";
+            };
+            gotoPreviousStart = {
+              "[m" = "@function.outer";
+              "[[" = "@class.outer";
+            };
+            gotoPreviousEnd = {
+              "[M" = "@function.outer";
+              "[]" = "@class.outer";
+            };
           };
-          gotoNextEnd = {
-            "]M" = "@function.outer";
-            "][" = "@class.outer";
-          };
-          gotoPreviousStart = {
-            "[m" = "@function.outer";
-            "[[" = "@class.outer";
-          };
-          gotoPreviousEnd = {
-            "[M" = "@function.outer";
-            "[]" = "@class.outer";
-          };
-        };
-        swap = {
-          enable = true;
-          swapNext = {
-            "<leader>a" = "@parameters.inner";
-          };
-          swapPrevious = {
-            "<leader>A" = "@parameter.outer";
+          swap = {
+            enable = true;
+            swapNext = {
+              "<leader>a" = "@parameters.inner";
+            };
+            swapPrevious = {
+              "<leader>A" = "@parameter.outer";
+            };
           };
         };
       };
