bd5aaaa05b16da76de4d8de52d0a15ac6087ad44
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-06-06 18:38:10 +0200

    refactor(chromium): captive-browser has since been removed as a home manager module

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -11,14 +11,12 @@ with lib;
   options = {
     home.chromium = {
       enable = mkEnableOption "chromium";
-      captive-browser.enable = mkEnableOption "captive-browser";
     };
   };
 
   config = mkIf cfg.enable {
     programs = {
       chromium.enable = true;
-      captive-browser.enable = mkIf cfg.captive-browser.enable;
     };
   };
 }
