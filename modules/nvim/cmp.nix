df4f371f1e311095773d843c46da7fb614ebef64
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-06-06 18:37:16 +0200

    fix(nvim-cmp): disable lsp document symbol while the licensing issue is resolved

--- modules/nvim/cmp.nix
+++ modules/nvim/cmp.nix
@@ -38,7 +38,7 @@
             { name = "dap"; }
             { name = "luasnip"; }
             { name = "nvim_lsp"; }
-            { name = "nvim_lsp_document_symbol"; }
+            # { name = "nvim_lsp_document_symbol"; } # FIXME: incorrectly listed as an unfree package as of 05d48760cd7b5f05a60e772220d377a10c02eddf4ec12e3f333e8cad1ac5ce3e
             { name = "nvim_lsp_signature_help"; }
             { name = "path"; }
             { name = "treesitter"; }
