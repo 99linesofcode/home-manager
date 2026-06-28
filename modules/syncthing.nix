410ca4813798f858d13776456c88fe2a2a2ece8d
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-06-28 15:50:21 +0200

    feat(syncthing): add fairphone device ID

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -24,12 +24,16 @@ with lib;
           "boox" = {
             id = "O7JFX2O-SXQ2PGL-JGEPNM4-L42XPWW-A45NXOO-SSFRFJK-UIAGCC7-C5MRBQP";
           };
+          "fairphone" = {
+            id = "MSDUW4D-EKKTYK5-7YRZQ7N-AME2PPV-LILA4D5-BMDQU7P-MVJ7CTO-EE62IAX";
+          };
         };
         folders = {
           "Obsidian" = mkIf (config.home.google-drive.enable && config.home.obsidian.enable) {
             path = "/home/${username}/Documents/Google Drive/Obsidian";
             devices = [
               "boox"
+              "fairphone"
             ];
           };
         };
